35004d99dd8e4bb179dca8273ae8feef99855c5a
Omid Reza Vasheghani Farahani <omidrezav@gmail.com>
2022-02-07 12:02:36 +0330

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name:       Wp Queue
+ * Plugin URI:        https://github.com/dizatech/wp_queue
+ * Description:       A wordpress plugin for managing queues
+ * Version:           0.9
+ * Author:            Dizatech
+ * Author URI:        https://dizatech.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ */
